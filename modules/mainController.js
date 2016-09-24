2fd2a2574e9c54200f51f6e2cb3e325a2c98fd54
Nikhil Tiware <nikhiltiware@gmail.com>
2016-09-24 14:53:18 -0400

    reviews seperated according to tones

--- modules/mainController.js
+++ modules/mainController.js
@@ -18,12 +18,15 @@ reviewApp.controller('mainController', function($scope, $rootScope, $http, $stat
     var getSentiments = function(review) {
         watsonService.async(review).then(function(response) {
             console.log(response.data);
+            var reviewSentimentsArray = response.data;
             $scope.data = JSON.stringify(response.data);
+            analyseReviews(reviewSentimentsArray.sentences_tone);
         });
     };
 
     var combineReview = function(reviewsArr) {
         var finalString = "";
+
         function joinReviewDesc(element, index, array) {
             finalString = finalString + element.reviewDesc;
         }
@@ -31,6 +34,29 @@ reviewApp.controller('mainController', function($scope, $rootScope, $http, $stat
         reviewsArr.forEach(joinReviewDesc);
         return finalString;
     };
+
+    var analyseReviews = function(reviewSentimentsArray) {
+        var seperatedTones = {
+            anger: [],
+            disgust: [],
+            fear: [],
+            joy: [],
+            sadness: [],
+        };
+
+        function segregateReviews(element, index, array) {
+            if ( !! element.tone_categories[0]) {
+                var tones = element.tone_categories[0].tones;
+                var maxEmotion = tones.reduce(function(prev, curr) {
+                    return prev.score > curr.score ? prev : curr;
+                });
+                seperatedTones[maxEmotion.tone_id].push(element);
+            }
+
+        }
+        reviewSentimentsArray.forEach(segregateReviews);
+        console.log(seperatedTones);
+    }
 
     init();
 });
