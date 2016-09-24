ff2bd0e57cfb35f9e648d5952105b85ce2b04563
Nikhil Tiware <nikhiltiware@gmail.com>
2016-09-24 13:52:16 -0400

    services for macys and watson implemented

---
+++ modules/macyService.js
@@ -0,0 +1,24 @@
+reviewApp.factory('macyService', function($http, $base64) {
+
+    var getReviews = {
+        async: function(productId, numberOfReviews) {
+            var reqForMacy = {
+                method: 'GET',
+                headers: {
+                    'x-macys-webservice-client-id': 'hackapalooza'
+                },
+                Accept: 'application/json',
+                url: 'http://api.macys.com/v3/catalog/reviews?productId='+productId+'&numberOfReviewsRequested='+numberOfReviews,
+            };
+
+            return $http(reqForMacy).success(function(data) {
+                return data;
+            }).error(function(data) {
+                return data;
+            });
+        }
+    };
+
+    return getReviews;
+
+});
--- modules/mainController.js
+++ modules/mainController.js
@@ -1,35 +1,36 @@
-reviewApp.controller('mainController', function($scope, $rootScope, $http, $stateParams, $state, $base64, watsonService) {
-    $scope.welcome = "Nikhil";
+reviewApp.controller('mainController', function($scope, $rootScope, $http, $stateParams, $state, $base64, watsonService, macyService) {
 
-    var getRatingDist = function() {
-        var reqForMacy = {
-            method: 'GET',
-            headers: {
-                'x-macys-webservice-client-id': 'hackapalooza'
-            },
-            Accept: 'application/json',
-            url: 'http://api.macys.com/v3/catalog/reviews?productId=199462&numberOfReviewsRequested=5',
-        };
-        
-        var auth = $base64.encode("8063a4e3-6e7f-460e-8234-bd8920dbf6ff:KdkZkPwKDnbN");
-        var headers = {"Authorization": "Basic " + auth};
-        var reqForWatson = {
-            method: 'POST',
-            headers: headers,
-            url: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19',
-            data: {
-                text: "I was hesitant to purchase this because I didn't think we'd use it enough.  I was wrong!  We love this mixer.  We also bought the food processor attachment - so glad we did.   My husband uses this more than I do!"
-            }
-        };
-        //$scope.data = watsonService.getSentiments("nikhil");
-        $http(reqForMacy).success(function(data) {
-            $scope.data = data;
-            console.log($scope.data);
-        }).error(function(data) {
-            $scope.data = data;
+
+    var init = function() {
+        getReviews(2334186, 5);
+    }
+    var getReviews = function(productId, numberOfReviews) {
+        macyService.async(productId, numberOfReviews).then(function(response) {
+            console.log(response.data);
+            var reviewsObject = response.data;
+            var combinedReviews = combineReview(reviewsObject.perReviewWrapper);
+            console.log(combinedReviews);
+            getSentiments(combinedReviews);
+        });
+
+    };
+
+    var getSentiments = function(review) {
+        watsonService.async(review).then(function(response) {
+            console.log(response.data);
+            $scope.data = JSON.stringify(response.data);
         });
     };
 
-    getRatingDist();
+    var combineReview = function(reviewsArr) {
+        var finalString = "";
+        function joinReviewDesc(element, index, array) {
+            finalString = finalString + element.reviewDesc;
+        }
 
+        reviewsArr.forEach(joinReviewDesc);
+        return finalString;
+    };
+
+    init();
 });
