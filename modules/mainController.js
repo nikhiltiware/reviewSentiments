f42ebed3af27777d37fa27db181951cee6a657be
Nikhil Tiware <nikhiltiware@gmail.com>
2016-09-24 22:48:37 -0400

    macy query fixed

--- modules/mainController.js
+++ modules/mainController.js
@@ -1,27 +1,46 @@
 reviewApp.controller('mainController', function($scope, $rootScope, $http, $stateParams, $state, $base64, watsonService, macyService) {
 
-  var seperatedTones = {
-      anger: [],
-      disgust: [],
-      fear: [],
-      joy: [],
-      sadness: [],
-  };
+    var seperatedTones = {
+        anger: [],
+        disgust: [],
+        fear: [],
+        joy: [],
+        sadness: [],
+    };
 
-  var aggregatedReviews = {
-    anger :"",
-    disgust = "";
-    fear = "";
-    joy = "";
-    sadness = "";
-  };
-
-
-
-
-    var init = function() {
-        getURL();
-        getReviews(2334186, 5);
+    var aggregatedReviews = {
+        anger: "",
+        disgust: "",
+        fear: "",
+        joy: "",
+        sadness: "",
+    };
+    
+    var getURL = function() {
+        chrome.tabs.query({
+                active: true,
+                windowId: chrome.windows.WINDOW_ID_CURRENT
+            },
+            function(array_of_Tabs) {
+                console.log(array_of_Tabs[0].url);
+                var url = array_of_Tabs[0].url;
+                var name = 'ID';
+                var paramValue;
+                if (!url) url = window.location.href;
+                name = name.replace(/[\[\]]/g, "\\$&");
+                var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                    results = regex.exec(url);
+                if (!results) paramValue = null;
+                if (!results[2]) paramValue = '';
+                paramValue = decodeURIComponent(results[2].replace(/\+/g, " "));
+                console.log(paramValue);
+                init(paramValue);
+            }
+        );
+    };
+    
+    var init = function(paramValue) {
+        getReviews(paramValue, 5);
     }
 
     var getReviews = function(productId, numberOfReviews) {
@@ -39,7 +58,7 @@ reviewApp.controller('mainController', function($scope, $rootScope, $http, $stat
         watsonService.async(review).then(function(response) {
             console.log(response.data);
             var reviewSentimentsArray = response.data;
-            $scope.data = JSON.stringify(response.data);
+
             analyseReviews(reviewSentimentsArray.sentences_tone);
         });
     };
@@ -72,29 +91,42 @@ reviewApp.controller('mainController', function($scope, $rootScope, $http, $stat
         }
         reviewSentimentsArray.forEach(segregateReviews);
         console.log(seperatedTones);
+        $scope.data = JSON.stringify(seperatedTones);
     }
 
-    var getURL = function(){
-      chrome.tabs.query({
-      	active: true,
-      	windowId: chrome.windows.WINDOW_ID_CURRENT
-         },
-         function(array_of_Tabs) {
-      	console.log(array_of_Tabs[0].url);
-      	var url = array_of_Tabs[0].url;
-      	var name = 'ID';
-      	var paramValue;
-      	if (!url) url = window.location.href;
-          name = name.replace(/[\[\]]/g, "\\$&");
-          var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-              results = regex.exec(url);
-          if (!results) paramValue= null;
-          if (!results[2]) paramValue ='';
-          paramValue= decodeURIComponent(results[2].replace(/\+/g, " "));
-      	console.log(paramValue);
-         }
-      );
+
+    var calculateDataForGraph = function() {
+        var avgScore = {
+            anger: 0,
+            disgust: 0,
+            fear: 0,
+            joy: 0,
+            sadness: 0,
+        };
+        for (var emotion in seperatedTones) {
+            //console.log("obj." + prop + " = " + obj[prop]);
+            avgScore[prop] = avgScore[prop] + getTotal(emotion,seperatedTones[emotion]);
+
+        }
+
+    }
+
+    var getTotal = function(emotion,arr) {
+        var finalTotal = 0;
+
+        function countScore(element, index, array) {
+            finalTotal = finalTotal + element.tone_categories[0].tones[emotion];
+        }
+
+        reviewsArr.forEach(countScore);
+        return finalTotal;
+
     };
 
-    init();
+    var setChartOptions = function() {
+
+
+    }
+    getURL();
+
 });
